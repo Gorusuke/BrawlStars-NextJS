b32e02fc93073c1c0c3e40caefb875742fa42901
Jose Andres Garcia <Gorusuke123@gmail.com>
2024-03-07 21:02:50 -0500

    Add pagination component

---
+++ src/ui/client/Pagination.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import Link from "next/link";
+import { usePathname, useSearchParams } from "next/navigation";
+import styles from '@/ui/global.module.css'
+
+const Pagination = ({totalPages}: {totalPages: number}) => {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const currentPage = Number(searchParams.get('page')) || 1;
+
+  const createPageURL = (pageNumber: number | string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', pageNumber.toString());
+    console.log(`${pathname}?${params.toString()}`)
+    return `${pathname}?${params.toString()}`;
+  };
+
+  const allPages = Array.from({length: totalPages}, (_, idx) => idx + 1)
+
+  return allPages.map(page => 
+    <Link href={createPageURL(page)} key={page}>
+      <button className={styles.paginationButton} style={{backgroundColor: page === currentPage ? '#ffe058b3' : 'gray'}}>
+        {page}
+      </button>
+    </Link>
+  )
+}
+
+export default Pagination
