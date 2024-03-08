4a0eefa8fa982f7e35aefb1566880f3ec5d1a40b
Jose Andres Garcia <Gorusuke123@gmail.com>
2024-03-07 21:02:11 -0500

    Add contants and css styles

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -7,16 +7,18 @@ export const NEXT = 'next'
 export const FIRST_BRAWLER_ID = '16000000'
 export const LAST_BRAWLER_ID = '16000079'
 export const UPCOMING = 'upcoming'
+export const LEFT_ARROW = "M21 12L3 12M3 12L11.5 3.5M3 12L11.5 20.5"
+export const RIGHT_ARROW = "M3 12L21 12M21 12L12.5 3.5M21 12L12.5 20.5"
 export const BUTTONS_ARR = [
   {
     text: "Prev Brawler",
-    path: "M21 12L3 12M3 12L11.5 3.5M3 12L11.5 20.5",
+    path: LEFT_ARROW,
     isUp: false,
     step: PREV
   },
   {
     text: "Next Brawler",
-    path: "M3 12L21 12M21 12L12.5 3.5M21 12L12.5 20.5",
+    path: RIGHT_ARROW,
     isUp: true,
     step: NEXT
   }
